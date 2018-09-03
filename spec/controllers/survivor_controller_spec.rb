b23665e0e19b28e1a5bebae2a5708d5051c03c2b
Leonardo <leo.mrisch@gmail.com>
2018-09-03 15:18:00 -0300

    update specs for the new endpoint

--- spec/controllers/survivor_controller_spec.rb
+++ spec/controllers/survivor_controller_spec.rb
@@ -74,4 +74,51 @@ RSpec.describe SurvivorController, type: :controller do
       end
     end
   end
+
+  describe "POST #update_location" do
+    let(:survivor) { create(:survivor) }
+    context "when pass valid and existent params" do
+      let(:valid_params) do
+        {
+          "survivor_id": survivor.id,
+          "latitude": "0.1533",
+          "longitude": "0.14444"
+        }
+      end
+      it "update survivor with new latitude/longitude" do
+        post :update_location, params: valid_params
+        expect(response.code).to eq("200")
+        expect(JSON.parse(response.body)['message']).to eq("Survivor updated")
+      end
+    end
+    context "when pass invalid survivor" do
+      let(:invalid_params) do
+        {
+          "survivor_id": 'inexistent',
+          "latitude": "0.1533",
+          "longitude": "0.14444"
+        }
+      end
+      it "update survivor with new latitude/longitude" do
+        post :update_location, params: invalid_params
+        expect(response.code).to eq("422")
+        expect(JSON.parse(response.body)['message']).to eq("Survivor not found")
+      end
+    end
+
+    context "when pass invalid latitude/longitude" do
+      let(:invalid_params) do
+        {
+          "survivor_id": survivor.id,
+          "latitude": "33333333333333",
+          "longitude": "33333333333333"
+        }
+      end
+      it "update survivor with new latitude/longitude" do
+        post :update_location, params: invalid_params
+        expect(response.code).to eq("400")
+        expect(JSON.parse(response.body)['message']).to eq("Fail to update survivor, insert a valid latitude and longitude")
+      end
+    end
+  end
 end
