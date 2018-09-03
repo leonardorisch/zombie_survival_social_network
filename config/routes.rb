4bb2d1a24ead46b99ef8c086d5741646e9469917
Leonardo <leo.mrisch@gmail.com>
2018-09-03 15:17:05 -0300

    route to update location of a existent survivor

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   post '/survivor', to: 'survivor#create', as: :create_survivor
+  post '/survivor/update_location', to: 'survivor#update_location', as: :update_location_survivor
 end
