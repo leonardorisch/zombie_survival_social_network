94ef25e3f541c20e8e14b6552c8750ed80068315
Leonardo <leo.mrisch@gmail.com>
2018-09-03 15:17:25 -0300

    add new location params and action to support new route

--- app/controllers/survivor_controller.rb
+++ app/controllers/survivor_controller.rb
@@ -5,10 +5,20 @@ class SurvivorController < ApplicationController
     ActiveRecord::Base.transaction do
       survivor = Survivor.create(survivor_params)
       raise ActionController::BadRequest.new() if survivor.new_record?
-      render json: SurvivorSerializer.new(survivor).serialized_json, status: 200
+      json_response(SurvivorSerializer.new(survivor).serialized_json, 200)
     end
   rescue StandardError => exception
-    error_response("Failed to create a new survivor #{exception}", 400)
+    json_response({ message: "Failed to create a new survivor #{exception}" }, 400)
+  end
+
+  def update_location
+    survivor = Survivor.find(location_params[:survivor_id])
+    survivor.update(location_params.except(:survivor_id))
+    json_response({ message: "Survivor updated" }, 200)
+  rescue ActiveRecord::RecordNotFound => exception
+    json_response({ message: "Survivor not found" }, 422)
+  rescue ActiveRecord::RangeError => exception
+    json_response({ message: "Fail to update survivor, insert a valid latitude and longitude" }, 400)
   end
 
   private
@@ -16,5 +26,9 @@ class SurvivorController < ApplicationController
   def survivor_params
     params.require(:survivor).permit(:name, :age, :gender, :latitude, :longitude,
       inventory_attributes: [ :id, :type ])
+  end
+
+  def location_params
+    params.permit(:survivor_id, :latitude, :longitude)
   end
 end
