25126ec68c0821fc2563ce6db003ffc8fe38b542
Leonardo <leo.mrisch@gmail.com>
2018-09-06 17:47:33 -0300

    add route to get survivor data

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   post '/survivor/update_location', to: 'survivor#update_location', as: :update_location_survivor
   post '/survivors/trade', to: 'inventory#trade', as: :survivors_trade_resource
   post '/survivor/report_infectation', to: 'infectation#report', as: :report_infectation_survivor
+  get '/reports', to: 'reports#show', as: :reports_survivors_data
 end
